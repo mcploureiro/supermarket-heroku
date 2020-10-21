f0f8e4ae02378cdb60a48357ca28e375b8aaa33d
miguel <ml@appradar.com>
2020-10-21 08:53:41 +0200

    test without the global.R

--- ui.R
+++ ui.R
@@ -1,4 +1,17 @@
 
+library (shiny)
+library (shinyjs)
+library (dplyr)
+library (ggplot2)
+library (grid)
+library (gridExtra)
+library (DT)
+
+myData <- read.csv ( "myJoin2.txt" ) 
+myPerson <- unique(myData$pe_name) #),"") #), levels=c(unique(myData$pe_name),"") )
+myFill <- c ( "chocolate3", "paleturquoise3", "seagreen4") #, "white")
+names ( myFill ) <- myPerson
+
 shinyUI (
     fluidPage(
         
